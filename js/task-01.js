d8e1f63ef1d36f5c5178b61e215eab24570f8b56
Oleksandr Kalienchenko <owheels.qa@gmail.com>
2023-05-17 11:13:19 +0300

    Task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,7 @@
 const list = document.querySelector("#categories");
-console.dir(list);
-console.dir(list.children);
 console.log("Number of categories:", list.children.length);
-let namCategories = list.children.length;
-console.log("Number of categories:", namCategories);
-
-// const item = document.querySelector("h2");
-// console.dir(item);
-
-[...list.children].forEach((item) => {console.dir(item);});
-
-const item = document.querySelector(".item");
-console.dir(item);
+const item = document.querySelectorAll(".item");
+[...item].forEach((item, idx) => {
+    console.log("Category:",item.firstElementChild.innerText);
+    console.log("Elements:", item.lastElementChild.childElementCount);
+});
