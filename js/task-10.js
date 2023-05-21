da4d364a984c7a35b563232c3d3a7395c46d9759
Oleksandr Kalienchenko <owheels.qa@gmail.com>
2023-05-21 19:09:15 +0300

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const klacKlac = document.querySelectorAll("button");
+[...klacKlac].forEach((item, idx) => {
+  console.log(item);
+  item.addEventListener("click", addText)
+  function addText(evt) {
+    console.log(evt);
+    alert("Я обов'язково зроблю це трошки пізніше \u{1F614}");
+  }
+});
+
+
