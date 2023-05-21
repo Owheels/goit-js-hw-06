f430ee6728503bd1d70701068bf04b1299f094b3
Oleksandr Kalienchenko <owheels.qa@gmail.com>
2023-05-21 16:46:32 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
-
+const formLogin = document.querySelector(".login-form");
+const emailInput = formLogin.elements["email"];
+const passwordInput = formLogin.elements["password"];
+formLogin.addEventListener("submit", formProcessing);
+function formProcessing(evt) {
+  evt.preventDefault();
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Please complete all fields.");
+  } else {
+      const {
+        elements: { login, password },
+      } = evt.currentTarget;
+    const user = {};
+    user[emailInput.name] = emailInput.value;
+      user[passwordInput.name] = passwordInput.value;
+      console.dir(user);
+      formLogin.reset();
+    };
+}
